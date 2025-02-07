e141ffdf4038cd0368003a99c0a6f785870c3c07
saurvkmr0 <saurvkmr0@gmail.com>
2025-02-08 03:04:26 +0530

    password hashing done

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/Login';
 import RegisteredUsers from './pages/RegisteredUsers';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home.js';
+import Signup from './pages/Singup.js';
 
 
 function App(){
@@ -12,6 +13,7 @@ function App(){
       <Router>
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/signup" element={<Signup />}/>
           <Route path="/login" element={<Login />}/>
           <Route path="/users" element={<RegisteredUsers />}/>
         </Routes>
