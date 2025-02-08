1ab3cb1a97be2348179ecc710b0700a99e862f61
saurvkmr0 <saurvkmr0@gmail.com>
2025-02-09 04:18:42 +0530

    authentication and password hashing is set on server side instead of client side

--- server/src/routes/usersRoute.js
+++ server/src/routes/usersRoute.js
@@ -1,12 +1,21 @@
 import express from 'express'
 import users from '../models/UserSchema.js'
+import bcrypt from 'bcryptjs';
 
 
 const usersRoute = express.Router();
 
 usersRoute.post('/users',async(req,res)=>{
     try {
-        const user = await users.create(req.body);
+        var salt = await bcrypt.genSalt(10);
+        var password = await bcrypt.hash(req.body.password, salt);
+        const data=req.body;
+        // console.log(data);
+        const updatedData = {
+            ...data,
+            password: password
+        }
+        const user = await users.create(updatedData);
         res.status(201).json(user);
     } catch (error) {
         if(error.code===11000){
@@ -25,9 +34,21 @@ usersRoute.post('/users',async(req,res)=>{
 
 usersRoute.get('/users',async(req,res)=>{
     try{
-        const user = await users.findOne({email:req.query.email},{password:1, _id:1});
-        console.log(user);
-        res.status(200).json(user);
+        const user = await users.findOne({email:req.query.email},{password:1, _id:1,email:1});
+        if(!user){
+            res.status(404).json("user not found");
+        }else{
+            bcrypt.compare(req.query.password,user.password)
+            .then((match)=>{
+                if(match){
+                    console.log("password matched");
+                    res.status(200).json(user.email);
+                }else{
+                    console.log("incorrect password");
+                    res.status(400);
+                }
+            })
+        }
     }catch(error){
         res.status(404).json(user);
     }
